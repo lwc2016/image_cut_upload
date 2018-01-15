4d79128cc24979849577122b30fc102f554f774c
刘文超 <liuwenchao1990@163.com>
2018-01-15 09:19:19 +0800

    提交最新代码

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const app = require("app");
+const app = express();
 
 /*---------配置静态资源路径---------*/
 app.use(express.static(path.join(__dirname, "public/")));
